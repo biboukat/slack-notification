c09e7aab90451672a1a80a7280cf728484d68915
alexander kovtun <kovtuna@getsorbet.com>
2024-07-22 17:41:10 +0200

    test7

--- dist/index.js
+++ dist/index.js
@@ -29193,27 +29193,74 @@ function wrappy (fn, cb) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(9689));
+const core = __importStar(__nccwpck_require__(9689));
 const github_1 = __importDefault(__nccwpck_require__(2706));
-try {
-    console.log('bla 1');
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core_1.default.getInput("who-to-greet");
-    console.log(`Hello ${nameToGreet}!`);
-    const time = new Date().toTimeString();
-    core_1.default.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github_1.default.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+process.on("unhandledRejection", handleError);
+main().catch(handleError);
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            console.log("bla 1");
+            // `who-to-greet` input defined in action metadata file
+            const nameToGreet = core.getInput("who-to-greet");
+            console.log(`Hello ${nameToGreet}!`);
+            const time = new Date().toTimeString();
+            core.setOutput("time", time);
+            // Get the JSON webhook payload for the event that triggered the workflow
+            const payload = JSON.stringify(github_1.default.context.payload, undefined, 2);
+            console.log(`The event payload: ${payload}`);
+        }
+        catch (error) {
+            console.log("bla error ------>", error);
+            console.log("bla error ------>", JSON.stringify(error));
+            core.setFailed(error.message);
+        }
+    });
 }
-catch (error) {
-    console.log("bla error ------>", error);
-    console.log("bla error ------>", JSON.stringify(error));
-    core_1.default.setFailed(error.message);
+function handleError(err) {
+    core.error(err);
+    if (err && err.message) {
+        core.setFailed(err.message);
+    }
+    else {
+        core.setFailed(`Unhandled Error: ${err}`);
+    }
 }
 
 
--- index.ts
+++ index.ts
@@ -1,19 +1,33 @@
-import core from "@actions/core";
+import * as core from "@actions/core";
 import github from "@actions/github";
 
-try {
-  console.log('bla 1');
-  
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error: any) {
-  console.log("bla error ------>", error);
-  console.log("bla error ------>", JSON.stringify(error));
-  core.setFailed(error.message);
+process.on("unhandledRejection", handleError);
+main().catch(handleError);
+
+async function main(): Promise<void> {
+  try {
+    console.log("bla 1");
+
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}!`);
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error: any) {
+    console.log("bla error ------>", error);
+    console.log("bla error ------>", JSON.stringify(error));
+    core.setFailed(error.message);
+  }
+}
+
+function handleError(err: Error): void {
+  core.error(err);
+  if (err && err.message) {
+    core.setFailed(err.message);
+  } else {
+    core.setFailed(`Unhandled Error: ${err}`);
+  }
 }
